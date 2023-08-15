7898f3482f235caae723282d5bf13a2c369b6908
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-08-15 18:16:04 +0200

    Dodaj pliki projektów.

---
+++ LockOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿int Balance = 100;
+
+var t1 = Task.Factory.StartNew(() =>
+{
+    Parallel.For(0, 10000, _ => Interlocked.Add(ref Balance, 100));
+
+});
+var t2 = Task.Factory.StartNew(() =>
+{
+    Parallel.For(0, 10000, _ => Interlocked.Add(ref Balance, -100));
+});
+Task.WaitAll(t1, t2);
+Console.WriteLine($"Balance is:{Balance}");
